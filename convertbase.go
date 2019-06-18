0c1e9cd0b8fb59b62e570cb21f6329511eef2544
Nelson <nelgoug@gmail.com>
2019-06-19 00:42:04 +0200

    convertbase

--- convertbase.go
+++ convertbase.go
@@ -1,6 +1,28 @@
 package piscine
 
+import "strings"
+
 func ConvertBase(nbr, baseFrom, baseTo string) string{
-	return PrintNbrBase(AtoiBase(nbr,baseFrom),baseTo)
+	return PrintNbrBase1(AtoiBase(nbr,baseFrom),baseTo)
+}
+
+func PrintNbrBase1(n int,base string) string{//convertir un int dans une base et retourne le string de celui-ci
+	if len(base)<2 || !uniquealphaandnosigne(base){
+		fmt.Print("NV")	
+	}else if base=="0123456789"{
+		return strings.Itoa(n)
+	}else{
+		str:=""
+		if n<0{
+			n=-n
+			str=Concat(str,"-")
+		}
+		ch:=basen(n,len(base))
+		for _,val:= range ch{
+			num,_:=strconv.Atoi(string(val))
+			str=Concat(str,string(base[num]))
+		}
+		return str
+	}
 }
 
