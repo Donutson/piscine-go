cace54704c7a696efcd414bad45bd4f1b4a2d919
Nelson <nelgoug@gmail.com>
2019-06-22 00:17:29 +0200

    sortwordarr

--- sortwordarr.go
+++ sortwordarr.go
@@ -4,7 +4,9 @@ func SortWordArr(array []string) {
 	ech:=""
 	for i:=0;i<len(array);i++{
 		for j:=i+1;j<len(array);j++{
-			if []byte(array[j])[0]<[]byte(array[i])[0]{
+			a:=[]byte(array[j])
+			b:=[]byte(array[i])
+			if a[0]<b[0]{
 				ech=array[i]
 				array[i]=array[j]
 				array[j]=ech
