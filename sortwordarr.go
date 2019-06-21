52e958e4ec3c4c8a541e20c61e1ecc7df10b72b3
Nelson <nelgoug@gmail.com>
2019-06-22 00:25:17 +0200

    sortwordarr

--- sortwordarr.go
+++ sortwordarr.go
@@ -6,7 +6,7 @@ func SortWordArr(array []string) {
 		for j:=i+1;j<len(array);j++{
 			a:=[]byte(array[j])
 			b:=[]byte(array[i])
-			if a[0]<b[0]{
+			if comparearrayascii(a, b){
 				ech=array[i]
 				array[i]=array[j]
 				array[j]=ech
@@ -15,3 +15,16 @@ func SortWordArr(array []string) {
 	}
 }
 
+func comparearrayascii(a, b []byte)bool{
+	if len(a)<=len(b){
+		n:=len(a)	
+	}else{
+		n:=len(b)
+	}
+	for i:=0;i<n;i++{
+		if b[i]<a[i]{
+			return false
+		}
+	}
+	return true
+}
