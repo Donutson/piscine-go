bf122b01fb286d1d4e316f1b3338c5e0d552bbac
Nelson <nelgoug@gmail.com>
2019-06-22 12:10:43 +0200

    rot14

--- rot14.go
+++ rot14.go
@@ -3,7 +3,22 @@ package piscine
 func Rot14(str string) string {
 	bstr:=[]byte(str)
 	for i:= range bstr{
-		bstr[i]+=14
+		fmt.Println("avant",bstr[i])
+		if bstr[i]>=65 && bstr[i]<=90{
+			if (bstr[i]+14)>90{
+				bstr[i]=bstr[i]-12			
+			}else{
+				bstr[i]+=14
+			}
+		}
+		if bstr[i]>=97 && bstr[i]<=122{
+			if (bstr[i]+14)>122{
+				bstr[i]=bstr[i]-12			
+			}else{
+				bstr[i]+=14
+			}
+		}
+		fmt.Println("apres",bstr[i])
 	}
 	return string(bstr)
 }
