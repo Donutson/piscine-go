0b1bf9364f4a54eaf815af4f74f38c06c98d66da
Nelson <nelgoug@gmail.com>
2019-06-22 22:13:29 +0200

    cat

---
+++ cat/cat.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "os"
+)
+
+func check(e error) {
+    if e != nil {
+        fmt.Println(e)
+    }
+}
+
+func read(filename string) string {
+    data, err := ioutil.ReadFile(filename)
+    check(err)
+    return string(data)
+}
+
+func main() {
+	filename:=os.Args
+	if len(filename)==1{
+		fmt.Println("File name missing")
+	}else{
+		for i:=1;i<len(filename);i++{
+			data:=read(filename[1])
+			fmt.Println(data)
+		}
+		
+	}
+}
