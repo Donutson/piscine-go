db10b5854ae618f7443a32c0d93d93e241fc1587
Nelson <nelgoug@gmail.com>
2019-06-22 14:04:55 +0200

    pilo

--- pilot/pilot.go
+++ pilot/pilot.go
@@ -1,6 +1,16 @@
 package main
 
-func main(){}
+import "fmt"
+
+func main(){
+	var donnie Pilot
+	donnie.Name = "Donnie"
+	donnie.Life = 100.0
+	donnie.Age = 24
+	donnie.Aircraft = AIRCRAFT1
+
+	fmt.Println(donnie)
+}
 
 const AIRCRAFT1=1
 type Pilot struct{
