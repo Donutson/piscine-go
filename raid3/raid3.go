9ce9fb694ff029735cca04cb192195db44dba666
Nelson <nelgoug@gmail.com>
2019-06-24 00:57:23 +0200

    raid3

--- raid3/raid3.go
+++ raid3/raid3.go
@@ -1,15 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"io/ioutil"
-	)
-
 func main(){
-	param,e:=ioutil.ReadFile("./student/raid1a.go")
-	if e!=nil{
-		fmt.Println(e)
-	}
-	fmt.Println(string(param))
+	
 }
 
