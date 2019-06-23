09d5ccb7e0b7f46050adb7da83c4b059d31dc0bb
Nelson <nelgoug@gmail.com>
2019-06-23 20:13:55 +0200

    raid3

--- raid3/raid3.go
+++ raid3/raid3.go
@@ -2,24 +2,9 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"io/ioutil"
 	)
 
-func check(e error) {
-    if e != nil {
-        fmt.Println(e)
-    }
-}
-
-func read(filename string) string {
-    data, err := ioutil.ReadFile(filename)
-    check(err)
-    return string(data)
-}
-
 func main(){
-	data:=read("raid1a.go")
-	fmt.Println(data)	
+	fmt.Println("boj")	
 }
 
