f7accf5c0871ceac16b8c3a74225e9905fe4c2ae
Nelson <nelgoug@gmail.com>
2019-06-23 19:45:42 +0200

    raid3

---
+++ raid3/raid3.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	//"os"
+	//"strings"
+	)
+func main(){
+	
+		fmt.Println("[raid1a] [12] [34] || [raid1b] [12] [34] || [raid1c] [12] [34] || [raid1d] [12] [34] || [raid1e] [12] [34]")
+	
+}
+
