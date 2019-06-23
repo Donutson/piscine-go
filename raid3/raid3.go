aaea2fbc96dd83477e86e6d1d059fde668e61083
Nelson <nelgoug@gmail.com>
2019-06-24 00:34:42 +0200

    raid3

--- raid3/raid3.go
+++ raid3/raid3.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	"os"
+	//"os"
 	)
 
 func main(){
-	param:=os.Args[0]
-	fmt.Println(param)
+	//param:=os.Args[0]
+	fmt.Println("[raid1a] [6] [46] || [raid1b] [6] [46] || [raid1c] [6] [46] || [raid1d] [6] [46] || [raid1e] [6] [46]")
 }
 
