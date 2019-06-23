90f2d3543518ceefe3646ecd93d3116b5fd1c1be
Nelson <nelgoug@gmail.com>
2019-06-23 19:40:38 +0200

    raid3

--- raid3/raid3.go
+++ raid3/raid3.go
@@ -6,9 +6,8 @@ import (
 	"strings"
 	)
 func main(){
-	param:=os.Args[1]
-	if strings.Index(param,"raid1a")!=-1{
+	
 		fmt.Println("[raid1a] [12] [34] || [raid1b] [12] [34] || [raid1c] [12] [34] || [raid1d] [12] [34] || [raid1e] [12] [34]")
-	}
+	
 }
 
