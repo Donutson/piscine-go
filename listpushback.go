b7b438e417d60de79370e45b3951ef9effabfada
Nelson <nelgoug@gmail.com>
2019-06-27 15:57:00 +0200

    listpushback

--- listpushback.go
+++ listpushback.go
@@ -14,13 +14,13 @@ func ListPushBack(l *List, data interface{}) {
 	n:=&NodeL{Data:data}
 	if l.Head==nil{
 		l.Head=n
-	}else if l.Tail==nil{
-		l.Head.Next=n
-		l.Tail=n
-		l.Tail.Next=nil
 	}else{
-		l.Tail.Next=n
-		l.Tail=n
+		elt:=l.Head
+		for elt.Next!=nil{
+			elt=elt.Next
+		}
+		elt.Next=n
 	}
 }
+
 
