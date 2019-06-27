34706a3fa93729476987b9cb82c29e53e0da1c74
Nelson <nelgoug@gmail.com>
2019-06-27 16:42:35 +0200

    clearlist

---
+++ clearlist.go
@@ -0,0 +1,21 @@
+package piscine
+
+func ListClear(l *List) {
+	if l.Head!=nil{
+		for l.Head.Next!=nil{
+			old:=l.Head
+			l.Head=l.Head.Next
+			old.Next=nil
+		}
+	}
+}
+
+func PrintList(l *List) {
+	link := l.Head
+	for link != nil {
+		fmt.Print(link.Data, " -> ")
+		link = link.Next
+	}
+	fmt.Println(nil)
+}
+
