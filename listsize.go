2cf5288d8c4fe7747afb345f8a8e7e72c95ebe8f
Nelson <nelgoug@gmail.com>
2019-06-27 16:25:57 +0200

    listsize

--- listsize.go
+++ listsize.go
@@ -1,12 +1,15 @@
 package piscine
 
 func ListSize(l *List) int {
-	count:=1
-	for l.Head.Next!=nil{
-		l.Head=l.Head.Next
-		count++
-	}
-	return count
+	if l.Head!=nil{
+		count:=1
+		for l.Head.Next!=nil{
+			l.Head=l.Head.Next
+			count++
+		}
+		return count
+	}	
+	return 0
 }
 
 
