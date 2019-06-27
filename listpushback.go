c4075312061b0d95dda7094ece36b3c39a0c3acd
Nelson <nelgoug@gmail.com>
2019-06-27 02:00:43 +0200

    listpushback

--- listpushback.go
+++ listpushback.go
@@ -12,22 +12,15 @@ type List struct {
 
 func ListPushBack(l *List, data interface{}) {
 	n:=&NodeL{Data:data}
-        if l.Head==nil{
-                l.Head=n
-        }else if l.Tail==nil{
-                head := l.Head
-		head.Next = n
-		l.Tail = n 
-        }else{
-		listItem := l.Tail
-        
-        	//parcours pour se mettre a la fin de la list
-        	for listItem.Next != nil {
-            		listItem = listItem.Next
-        	}
-        
-        	//enregistrement du dernier element a la liste
-        	listItem.Next = n
-        	l.Tail = n
+	if l.Head==nil{
+		l.Head=n
+	}else if l.Tail==nil{
+		l.Head.Next=n
+		l.Tail=n
+		l.Tail.Next=nil
+	}else{
+		l.Tail.Next=n
+		l.Tail=n
 	}
 }
+
