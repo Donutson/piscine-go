1579a34803865ecae10e89457779a32f14ed582c
Nelson <nelgoug@gmail.com>
2019-06-28 19:10:04 +0200

    listfind

---
+++ listfind.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main(){
+
+}
+
+func CompStr(a, b interface{}) bool {
+	return a == b
+}
+
+func ListFind(l *List, ref interface{}, comp func(a, b interface{}) bool) *interface{} {
+	if l.Head!=nil{
+		cl:=l.Head
+		for cl.Next!=nil{
+			if comp(cl.Data,ref){
+				return cl
+			}
+			cl=cl.Next
+		}
+		if comp(cl.Data,ref){
+			return cl
+		}
+	}
+	return nil
+}
