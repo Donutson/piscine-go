d3b26ca5ba261b4d8792ec2371136fd9307526e2
Nelson <nelgoug@gmail.com>
2019-06-28 23:04:49 +0200

    removeif

--- listremoveif.go
+++ listremoveif.go
@@ -1,22 +1,28 @@
 package piscine
 
-
 func ListRemoveIf(l *List, data_ref interface{}) {
-		/*for l.Head!=nil{
+		for l.Head!=nil{
 			if l.Head.Data==data_ref{
 				l.Head=l.Head.Next
 			}else{
 				break
 			}		
-		}*/
-	/*if l.Head!=nil{
-		cl:=l.Head
-		for cl.Next!=nil{
-			if cl.Next.Data==data_ref{
-				cl.Next=cl.Next.Next
-			}
-			cl=cl.Next
 		}
-	}*/
+	if l.Head!=nil{
+		old:=l.Head
+		cur:=l.Head
+		for cur.Next!=nil{
+			if cur.Data==data_ref{
+				old.Next=cur.Next
+			}else{
+				old=cur
+			}
+			cur=cur.Next
+		}
+		if cur.Data==data_ref{
+			old.Next=cur.Next
+		}else{
+			old=cur
+		}
+	}
 }
-
